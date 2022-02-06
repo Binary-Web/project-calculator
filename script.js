f4b26d9eba08a46673ca0931a43edd96883b46e7
ben <ben.carlo.cornico@gmail.com>
2022-02-05 23:45:44 -0800

    logical in screen

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("javascript working")
+//NUM BUTTONS
+document.addEventListener("keypress", (e) => {
+    onKeyPress(e.keyCode);
+
+})
+
+function onKeyPress(key) {
+    if(key >= 65 || key <= 90) {
+        console.log("good")
+    }
+}
+
+
+//backspace             8
+//period                190
+// forward slash (/)    191
+//arithmetic operators 106 - 111
+//letters
+//      lowercase       65-90
