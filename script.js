def5a980a5d4418e54b40f3c831afbcfa432d2f3
ben <ben.carlo.cornico@gmail.com>
2022-02-10 23:09:28 -0800

    click event and UI updates

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+const calculator = document.querySelector('.calculator');
+const buttons = document.querySelector('.calc-btn');
 
-
-
-
+buttons.addEventListener('click', e => {
+    const key = e.target;
+    const action = key.dataset.action;
+    if(!action) {
+        console.log("num!");
+    } else if (
+                action === "add" ||
+                action === "subtract" ||
+                action === "multiply" ||
+                action === "divide"
+    ) {
+        console.log("operator key");
+    } else if (action === "decimal") {
+        console.log("decimal key");
+    } else if (action === "calculate") {
+        console.log("calculate key")
+    } else if (action === "clear") {
+        console.log("clear key")
+    }
+    
+})
